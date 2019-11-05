ee1a07e44ff1eaacca10059b75ca22ed2d40bcfa
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-05 17:52:55 -0500

    Initial Commit

---
+++ Static TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Static TableView
+//
+//  Created by Ankita Jain on 2019-11-05.
+//  Copyright © 2019 Ankita Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
